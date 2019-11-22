7511ed5f82f9fdf9183d399d976e76a833dd82de
Fernando Henrique Dalua <dalua_fernando@hotmail.com>
2019-11-22 17:51:09 -0300

    Cadastre-se

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -103,9 +103,7 @@ module.exports = app => {
 		}
 	});
 	
-	app.post('/post', upload.single('file'), (request, response) => {
-		let file = request.file
-		file.filename = ''
+	app.post('/post', upload.single('file'), (request, response) => {		
 		let message = request.body.message;
 		let id_user = request.session.id_user;
 		
@@ -115,20 +113,23 @@ module.exports = app => {
 			if (error){
 				response.send('Erro: '+error +' '+ id_user +' '+ message +' '+ userQuery);
 			}
-			let id_publication = results.insertId;			
-			let userPhoto = "INSERT INTO photo_publications (id_publication, photo) values ("+id_publication+", '"+file.filename+"')";
-			db.query(userPhoto, (error, results) => {
-				if (error){
-					response.send('Erro: '+error +' '+ id_publication +' '+ file.filename +' '+ userQuery);
-				}
-				let feedQuery = "SELECT c.fullname, date_format(p.date_post, '%d/%m/%Y %H:%m:%s') as date_post, p.post, f.photo FROM publications p inner join accounts c on p.id_account = c.id left join photo_publications f on p.id = f.id_publication order by p.date_post desc";		
-				db.query(feedQuery, (error, results) => {
-					feed = results;
-				});
-				setTimeout(function() {
-					response.render('home', {account: account, feed: feed});
-				}, 2000);				
-			});						
+			let id_publication = results.insertId;
+			if (request.file){
+				let file = request.file
+				let userPhoto = "INSERT INTO photo_publications (id_publication, photo) values ("+id_publication+", '"+file.filename+"')";
+				db.query(userPhoto, (error, results) => {
+					if (error){
+						response.send('Erro: '+error +' '+ id_publication +' '+ file.filename +' '+ userQuery);
+					}
+					let feedQuery = "SELECT c.fullname, date_format(p.date_post, '%d/%m/%Y %H:%m:%s') as date_post, p.post, f.photo FROM publications p inner join accounts c on p.id_account = c.id left join photo_publications f on p.id = f.id_publication order by p.date_post desc";		
+					db.query(feedQuery, (error, results) => {
+						feed = results;
+					});
+					setTimeout(function() {
+						response.render('home', {account: account, feed: feed});
+					}, 2000);				
+				});						
+			}
 		});		
 	});
 }
