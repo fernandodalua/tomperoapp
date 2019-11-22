aa27e2fbfc3c776e383da518d022349530e64998
Fernando Henrique Dalua <dalua_fernando@hotmail.com>
2019-11-22 17:19:40 -0300

    Cadastre-se

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -54,6 +54,34 @@ module.exports = app => {
 			}			
 		});
 	});
+	
+	app.post('/home', function(request, response) {		
+		let id_user = request.session.id_user;
+		
+		let userQuery = "SELECT a.id as id_user, a.username, a.password, a.email, a.fullname, a.sex, YEAR(FROM_DAYS(TO_DAYS(NOW())-TO_DAYS(a.birthday))) AS idade, a.description, p.profile, a.photo FROM accounts a inner join profile p on a.id_profile = p.id WHERE a.id = '"+ id_user +"'";
+		let feedQuery = "SELECT c.fullname, date_format(p.date_post, '%d/%m/%Y %H:%m:%s') as date_post, p.post, f.photo FROM publications p inner join accounts c on p.id_account = c.id left join photo_publications f on p.id = f.id_publication order by p.date_post desc";
+		
+		db.query(feedQuery, (error, results) => {
+			feed = results;
+		});
+		
+		if (username && password) {
+			db.query(userQuery, (error, results) => {
+				if (results.length > 0) {
+					request.session.loggedin = true;
+					request.session.username = username;
+					request.session.id_user = results[0].id_user;
+					account = results;
+					response.render('home', {account: results, feed: feed});
+				} else {
+					response.send('Incorrect Username and/or Password!');
+				}				
+			});
+		} else {
+			response.send('Please enter Username and Password!');
+			response.end();
+		}
+	});
 
 	app.post('/auth', function(request, response) {
 		let username = request.body.username;
