114d1128618729baba90a0bb879b3ed702b0bddb
Fernando Henrique Dalua <dalua_fernando@hotmail.com>
2019-11-22 14:03:03 -0300

    Post

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -44,8 +44,9 @@ module.exports = app => {
 		let message = request.body.message;
 		let id_user = request.session.id;
 		
-		let userQuery = "INSERT INTO publications (id_account, date_post, post) values ('"+id_user+"', now(), '"+message+"')"
+		let userQuery = "INSERT INTO publications (id_account, date_post, post) values ('"+id_user+"', NOW(), '"+message+"')"
 		db.query(userQuery, (error, results) => {
+			response.send('Incorrect Username and/or Password!'+error);
 			response.render('home', {account: account});
 		});		
 	});
