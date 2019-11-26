88db09521e5c664b1f9ea6a16702d9b6e81207c9
fernandodalua <dalua_fernando@hotmail.com>
2019-11-26 00:27:13 -0300

    Ajuste dos controllers

--- config/database.js
+++ config/database.js
@@ -1,8 +1,20 @@
 const mysql = require('mysql');
+const multer = require('multer');
 
-var connection = mysql.createConnection({
-	host     : 'localhost',
-	user     : 'root',
-	password : 'dalua123',
-	database : 'tompero'
+var storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, 'public/img/')
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.fieldname + '-' + Date.now())
+	}
+});
+
+var upload = multer({ storage: storage })
+
+var db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'dalua123',
+    database : 'tompero'
 });
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,23 +1,4 @@
-const mysql = require('mysql');
-const multer = require('multer');
-
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'public/img/')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now())
-  }
-});
-
-var upload = multer({ storage: storage })
-
-var db = mysql.createConnection({
-        host     : 'localhost',
-        user     : 'root',
-        password : 'dalua123',
-        database : 'tompero'
-});
+const database = require('.config/database');
 
 let userController = {};
 let profile = {};
