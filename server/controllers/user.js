79d0a827d54c5bfe9bd32e3991d35083804135d3
fernandodalua <dalua_fernando@hotmail.com>
2019-11-26 00:50:42 -0300

    Ajuste dos controllers

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -36,22 +36,26 @@ userController.authNew = (request, response) => {
 }
 
 userController.home = (request, response) => {
-	let id_user = request.session.id_user;
+	if (request.session.loggedin) {
+		let id_user = request.session.id_user;
+			
+		let userQuery = "SELECT a.id as id_user, a.username, a.password, a.email, a.fullname, a.sex, YEAR(FROM_DAYS(TO_DAYS(NOW())-TO_DAYS(a.birthday))) AS idade, a.description, p.profile, a.photo FROM accounts a inner join profile p on a.id_profile = p.id WHERE a.id = '"+ id_user +"'";
+		let feedQuery = "SELECT c.fullname, date_format(p.date_post, '%d/%m/%Y %H:%m:%s') as date_post, p.post, f.photo FROM publications p inner join accounts c on p.id_account = c.id left join photo_publications f on p.id = f.id_publication order by p.date_post desc";
 		
-	let userQuery = "SELECT a.id as id_user, a.username, a.password, a.email, a.fullname, a.sex, YEAR(FROM_DAYS(TO_DAYS(NOW())-TO_DAYS(a.birthday))) AS idade, a.description, p.profile, a.photo FROM accounts a inner join profile p on a.id_profile = p.id WHERE a.id = '"+ id_user +"'";
-	let feedQuery = "SELECT c.fullname, date_format(p.date_post, '%d/%m/%Y %H:%m:%s') as date_post, p.post, f.photo FROM publications p inner join accounts c on p.id_account = c.id left join photo_publications f on p.id = f.id_publication order by p.date_post desc";
-	
-	db.query(feedQuery, (error, results) => {
-		feed = results;
-	});
-	
-	db.query(userQuery, (error, results) => {
-		if (results.length > 0) {					
-			response.render('home', {account: results, feed: feed});
-		} else {
-			response.send('Incorrect Username and/or Password!');
-		}				
-	});
+		db.query(feedQuery, (error, results) => {
+			feed = results;
+		});
+		
+		db.query(userQuery, (error, results) => {
+			if (results.length > 0) {					
+				response.render('home', {account: results, feed: feed});
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}				
+		});
+	}else{
+		res.render('index')
+	}
 }
 
 userController.auth = (request, response) => {
@@ -84,40 +88,44 @@ userController.auth = (request, response) => {
 }
 
 userController.post = (request, response) => {
-	let message = request.body.message;
-	let id_user = request.session.id_user;
-	
-	let userQuery = "INSERT INTO publications (id_account, date_post, post) values ("+id_user+", NOW(), '"+message+"')";
-	
-	db.query(userQuery, (error, results) => {
-		if (error){
-			response.send('Erro: '+error +' '+ id_user +' '+ message +' '+ userQuery);
-		}
-		let id_publication = results.insertId;
-		if (request.file){
-			let file = request.file
-			let userPhoto = "INSERT INTO photo_publications (id_publication, photo) values ("+id_publication+", '"+file.filename+"')";
-			db.query(userPhoto, (error, results) => {
-				if (error){
-					response.send('Erro: '+error +' '+ id_publication +' '+ file.filename +' '+ userQuery);
-				}
-				let feedQuery = "SELECT c.fullname, date_format(p.date_post, '%d/%m/%Y %H:%m:%s') as date_post, p.post, f.photo FROM publications p inner join accounts c on p.id_account = c.id left join photo_publications f on p.id = f.id_publication order by p.date_post desc";		
-				db.query(feedQuery, (error, results) => {
-					feed = results;
-				});
-				setTimeout(function() {
-					response.render('home', {account: account, feed: feed});
-				}, 2000);				
-			});						
-		}
-		let feedQuery = "SELECT c.fullname, date_format(p.date_post, '%d/%m/%Y %H:%m:%s') as date_post, p.post, f.photo FROM publications p inner join accounts c on p.id_account = c.id left join photo_publications f on p.id = f.id_publication order by p.date_post desc";		
-		db.query(feedQuery, (error, results) => {
-			feed = results;
+	if (request.session.loggedin) {
+		let message = request.body.message;
+		let id_user = request.session.id_user;
+		
+		let userQuery = "INSERT INTO publications (id_account, date_post, post) values ("+id_user+", NOW(), '"+message+"')";
+		
+		db.query(userQuery, (error, results) => {
+			if (error){
+				response.send('Erro: '+error +' '+ id_user +' '+ message +' '+ userQuery);
+			}
+			let id_publication = results.insertId;
+			if (request.file){
+				let file = request.file
+				let userPhoto = "INSERT INTO photo_publications (id_publication, photo) values ("+id_publication+", '"+file.filename+"')";
+				db.query(userPhoto, (error, results) => {
+					if (error){
+						response.send('Erro: '+error +' '+ id_publication +' '+ file.filename +' '+ userQuery);
+					}
+					let feedQuery = "SELECT c.fullname, date_format(p.date_post, '%d/%m/%Y %H:%m:%s') as date_post, p.post, f.photo FROM publications p inner join accounts c on p.id_account = c.id left join photo_publications f on p.id = f.id_publication order by p.date_post desc";		
+					db.query(feedQuery, (error, results) => {
+						feed = results;
+					});
+					setTimeout(function() {
+						response.render('home', {account: account, feed: feed});
+					}, 2000);				
+				});						
+			}
+			let feedQuery = "SELECT c.fullname, date_format(p.date_post, '%d/%m/%Y %H:%m:%s') as date_post, p.post, f.photo FROM publications p inner join accounts c on p.id_account = c.id left join photo_publications f on p.id = f.id_publication order by p.date_post desc";		
+			db.query(feedQuery, (error, results) => {
+				feed = results;
+			});
+			setTimeout(function() {
+				response.render('home', {account: account, feed: feed});
+			}, 2000);
 		});
-		setTimeout(function() {
-			response.render('home', {account: account, feed: feed});
-		}, 2000);
-	});
+	}else{
+		res.render('index')
+	}
 }
 
 module.exports = userController;
