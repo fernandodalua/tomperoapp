5c8dff7d02adf4677a034b71909a9de8d1c1d821
Fernando Henrique Dalua <dalua_fernando@hotmail.com>
2019-11-27 15:27:04 -0300

    Ajustes dos controllers

--- config/database.js
+++ config/database.js
@@ -9,5 +9,16 @@ module.exports = () => {
 		database : 'tompero'
 	});
 	
+	const storage = multer.diskStorage({
+		destination: function (req, file, cb) {
+			cb(null, 'public/img/')
+		},
+		filename: function (req, file, cb) {
+			cb(null, file.fieldname + '-' + Date.now())
+		}
+	});
+
+	const upload = multer({ storage: storage })
+	
 	return db;
 }
--- config/express.js
+++ config/express.js
@@ -7,18 +7,7 @@ const fs = require('fs');
 
 
 module.exports = () => {
-	const app = express();
-
-	const storage = multer.diskStorage({
-		destination: function (req, file, cb) {
-			cb(null, 'public/img/')
-		},
-		filename: function (req, file, cb) {
-			cb(null, file.fieldname + '-' + Date.now())
-		}
-	});
-
-	const upload = multer({ storage: storage })
+	const app = express();	
 
 	app.use(session({
 		secret: 'secret',
