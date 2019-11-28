06cf4c5645d88eb6bb67964dda0c66ab1a5241bf
Fernando Henrique Dalua <dalua_fernando@hotmail.com>
2019-11-28 11:25:17 -0300

    Profile

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -153,10 +153,10 @@ module.exports = app => {
         let profileUserQuery = "SELECT c.id as id_account, c.fullname, date_format(p.date_post, '%d/%m/%Y %H:%m:%s') as date_post, p.post, f.photo FROM publications p inner join accounts c on p.id_account = c.id left join photo_publications f on p.id = f.id_publication where c.id = "+id_account+" order by p.date_post desc"
         db.query(profileUserQuery, (error, results) => {
             if (error) {
-                response.send('Erro: ' + error + ' ' + id_account + ' ' + profileUserQuery);
+                res.send('Erro: ' + error + ' ' + id_account + ' ' + profileUserQuery);
             }
             feedUser = results;
         });
-        response.render('profileUser', { feedUser: feedUser });
+        res.render('profileUser', { feedUser: feedUser });
     });
 }
