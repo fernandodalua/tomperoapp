e085434edd379d534b42118191797bf8fb93c03f
Fernando Henrique Dalua <dalua_fernando@hotmail.com>
2019-12-04 14:58:47 -0300

    Ajustes Quill

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -118,8 +118,7 @@ module.exports = app => {
 	
 	app.post('/post', upload.single('file'), (request, response) => {
 		if (request.session.loggedin) {
-            let message = request.body.message;
-            let message = message.substr(1);
+            let message = request.body.message.substr(1);            
             let id_user = request.session.id_user;            
 
 			let userQuery = "INSERT INTO publications (id_account, date_post, post) values ("+id_user+", NOW(), '"+message+"')";
