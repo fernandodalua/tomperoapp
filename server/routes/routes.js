ec536663dd7710eacfa1d8a805fd1fc0b23385f2
fernandodalua <dalua_fernando@hotmail.com>
2019-12-06 19:31:21 -0300

    Ajustes finais

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -228,7 +228,7 @@ module.exports = app => {
             } else {
                 let userQuery = "SELECT a.id as id_user, a.username, a.password, a.email, a.fullname, a.sex, YEAR(FROM_DAYS(TO_DAYS(NOW())-TO_DAYS(a.birthday))) AS idade, a.description, p.profile, a.photo FROM accounts a inner join profile p on a.id_profile = p.id WHERE a.id = '" + id_account + "'";
                 db.query(userQuery, (error, resultUser) => {
-                    res.render('profileUsers', { feedUser: results, accountUser: resultUser });    
+                    res.render('profileUsers', { feedUser: results, accountUser: resultUser, news: news });    
                 });
             }                        
         });        
