9bb1e8c39439876e48bcc45606f49db6aeafcc91
Fernando Henrique Dalua <dalua_fernando@hotmail.com>
2019-12-06 16:13:23 -0300

    feature receitas

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -285,6 +285,19 @@ module.exports = app => {
     });
 
     app.get('/recipe', (request, response) => {
-        response.render('recipe');
+        let id_user = request.session.id_user;
+        
+        let feedQuery = "SELECT c.id as id_account, c.fullname, date_format(p.date_post, '%d/%m/%Y %H:%m:%s') as date_post, p.post, f.photo, p.title, p.portion, p.preparation_time FROM publications p inner join accounts c on p.id_account = c.id left join photo_publications f on p.id = f.id_publication order by p.date_post desc";
+
+        db.query(feedQuery, (error, results) => {
+            for (var i = 0; i < results.length; i++) {
+                /*let converter = convertDeltaToHtml(results[i].post);
+                console.log(converter);
+                results[i].post = converter;*/
+            }
+            feed = results;
+        });
+
+        response.render('recipe', { feed: feed });
     });
 }
