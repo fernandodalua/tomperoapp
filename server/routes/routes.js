16c1a548de815914877b20a47c0228f52d4e56ed
Fernando Henrique Dalua <dalua_fernando@hotmail.com>
2019-12-06 17:00:10 -0300

    feature receitas

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -300,4 +300,11 @@ module.exports = app => {
 
         response.render('recipe', { feed: feed });
     });
+
+    app.get('/recipe/:id_publication', (req, res) => {
+        var id_publication = req.params.id_publication;
+
+        console.log(id_publication);
+        response.render('recipe', { feed: feed });
+    });
 }
